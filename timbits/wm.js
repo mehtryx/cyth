5a1d9b30e514f1cfd2ec5440e15050187873fc11
Keith Benedict <keith@benedicthome.com>
2014-12-01 09:23:57 -0500

    updated label

--- timbits/wm.js
+++ timbits/wm.js
@@ -136,6 +136,7 @@ timbit.eat = function(req, res, context) {
 		context.data = data;
 		context.color = 'Color,' + context.color;
 		context.type = 'Type,' + context.type;
+		context.showlabel = 'LabelShow,1';
 		res.setHeader("Content-Type", "text/plain");
 		timbit.render( req, res, context );
 	} );
